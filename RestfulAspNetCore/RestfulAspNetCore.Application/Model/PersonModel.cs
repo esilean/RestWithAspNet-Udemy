a06d962b2610e14db83fa706612657e82a25634f
esilean <le.bevilaqua@gmail.com>
2018-09-07 20:51:17 -0300

    implementing all verbs

---
+++ RestfulAspNetCore/RestfulAspNetCore.Application/Model/PersonModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace RestfulAspNetCore.Application.Model
+{
+    public class PersonModel
+    {
+
+        public PersonModel()
+        {
+        }
+        public long Id
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Gender
+        {
+            get;
+            set;
+        }
+    }
+}
